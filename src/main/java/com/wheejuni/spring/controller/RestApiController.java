8811daaeb502b3024c5345a2675a0a7517929bac
wheejuni <wheejuni@gmail.com>
2017-08-17 09:56:37 +0900

    REST API를 통한 조회 기능 추가

--- src/main/java/com/wheejuni/spring/controller/RestApiController.java
+++ src/main/java/com/wheejuni/spring/controller/RestApiController.java
@@ -1,5 +1,35 @@
 package com.wheejuni.spring.controller;
 
-public class RestApiController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.wheejuni.spring.domain.Redirection;
+import com.wheejuni.spring.domain.Request;
+import com.wheejuni.spring.handler.ShortenedUrlGenerationHandler;
+import com.wheejuni.spring.repositories.RedirectionRepository;
+
+@RestController("/api")
+public class RestApiController {
+	
+	@Autowired
+	RedirectionRepository redirectionRepo;
+	
+	@PostMapping("/new")
+	public Redirection setNewRedirectionRestApi(Request request) {
+		Redirection redirect = ShortenedUrlGenerationHandler.newRedirectionMaker(request);
+		redirectionRepo.save(redirect);
+		return redirect;
+	}
+	
+	@GetMapping("/{mappedurl}")
+	public Redirection getRedirectionInfo(@PathVariable String mappedurl) {
+		
+		Redirection requestedRedirect = redirectionRepo.findByMappedUrl(mappedurl);
+		return requestedRedirect;
+		
+	}
 }
